a8c4ac240872ffff6308262c60cec74efb9fc44c
gustavoGalarreta <gustavo.galarreta@pucp.pe>
2017-05-18 15:37:58 -0500

    fixing db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170518171334) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20170518171334) do
     t.integer  "activity_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["activity_id"], name: "index_user_activities_on_activity_id"
-    t.index ["user_id"], name: "index_user_activities_on_user_id"
+    t.index ["activity_id"], name: "index_user_activities_on_activity_id", using: :btree
+    t.index ["user_id"], name: "index_user_activities_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,9 +69,9 @@ ActiveRecord::Schema.define(version: 20170518171334) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["section_id"], name: "index_users_on_section_id"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["section_id"], name: "index_users_on_section_id", using: :btree
   end
 
 end
